6eeebe94d31bc1198e2be464f94c23a3e2f5a98a
aamroun <aamroun@f1br2s3.paris.42.school>
2026-08-26 09:12:50 +0200

    initialisation des variable de mesure

--- training.py
+++ training.py
@@ -11,8 +11,15 @@ with open("data.csv", 'r') as file:
     for row in csvreader:
         mileages.append(float(row[0]))
         prices.append(float(row[1]))
-    for i in mileages:
-        print (i)
-    for i in prices:
-        print (i)
-
+theta0 = float(0)
+theta1 = 0.0
+learningRate = 0.05
+iterations = 1000
+def estimatePrice(mileage):
+    Eprice = theta0 + (theta1 * mileage)
+    return (Eprice)
+#for i in mileages:
+ #   print (i)
+ #  print('\n')
+ #  for i in prices:
+ #      print (i)
